892f54cc0de1945e164c0d3a32d77b3b8e22d719
matibe95 <matibel2005@gmail.com>
2023-02-15 00:42:41 -0300

    Making Chat's Layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        typing: 'blink 1s steps(5, start) infinite'
+      },
+      keyframes: {
+        blink: {
+          to: { visibilty: 'hidden' }
+        }
+      },
+      colors: {
+        gptlogo: '#10a37f',
+        gptdarkgray: '#202123',
+        gptgray: '#343541',
+        gptlightgray: '#444654',
+      }
+    },
   },
   plugins: [],
 }
